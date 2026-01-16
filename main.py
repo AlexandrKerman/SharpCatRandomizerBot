6a18a3352b24e7edb23e7b07ab6ea3177420c1f5
Kerbin Original <aakolomeycev@gmail.com>
2026-01-16 15:50:18 +0300

    fixed output

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from itertools import chain
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from aiogram.types import FSInputFile
 import asyncio
 from dotenv import load_dotenv
 import os
 from pathlib import Path
 import random
+from src import text_source
 
 load_dotenv()
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
-
 images_folder = Path('image_src')
+text_list = text_source.get_text_list()
+
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=[[types.KeyboardButton(text='Сгенерировать')],],
+        resize_keyboard=True
+    )
+    await message.answer('Жопа', reply_markup=keyboard)
 
 
 def image_gen():
@@ -23,13 +34,19 @@ def image_gen():
     return photo
 
 
+def text_gen():
+    text = random.choice(text_list)
+    return text
+
+
 @dp.message()
 async def send_image(message: types.Message):
     print(message.text)
     chat_id = message.chat.id
     photo = image_gen()
+    text = text_gen()
     print(photo)
-    await bot.send_photo(chat_id=chat_id, photo=photo, caption='Жопа')
+    await bot.send_photo(chat_id=chat_id, photo=photo, caption=text)
 
 
 async def main():
